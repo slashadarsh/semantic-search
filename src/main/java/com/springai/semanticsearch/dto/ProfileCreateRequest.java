3a522f7b58cbaed0faa22a13e96aecc507cc5d37
slashadarsh <87933027+slashadarsh@users.noreply.github.com>
2025-03-31 14:22:06 +0530

    Add files via upload

---
+++ src/main/java/com/springai/semanticsearch/dto/ProfileCreateRequest.java
@@ -0,0 +1,20 @@
+package com.springai.semanticsearch.dto;
+
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProfileCreateRequest {
+    private String name;
+
+    private String description;
+
+    private String expression;
+
+    @Enumerated
+    private SetTopBoxType boxType;
+}
